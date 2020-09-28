5fdd29e9be8a38bf690f485fdea479509451d0b3
Fabian Maurer <dark.shadow4@web.de>
2020-09-29 01:03:13 +0200

    t2, ask felix?

--- src/main/java/de/darkshadow44/compatibility/sandbox/v1_7_10/net/minecraft/block/Compat_Block.java
+++ src/main/java/de/darkshadow44/compatibility/sandbox/v1_7_10/net/minecraft/block/Compat_Block.java
@@ -23,20 +23,18 @@ public class Compat_Block extends Block {
 		super(materialIn);
 	}
 	
+	// Can only be called from 1.12.2 and only when block is from 1.7.10
 	@Override
 	public boolean isNormalCube(IBlockState state, IBlockAccess world, BlockPos pos)
 	{
-		if (original == null) // Owned by 1.7.10
-			return Compat_isNormalCube(world, pos.getX(), pos.getY(), pos.getZ());
-		else
-			return super.isNormalCube(state, world, pos);
+		return Compat_isNormalCube(world, pos.getX(), pos.getY(), pos.getZ());
 	}
 
 	// Can be overriden by 1.7.10 mod
 	public boolean Compat_isNormalCube(IBlockAccess world, int x, int y, int z) {
-		if (original == null) // Owned by 1.7.10
+		if (original == null) // Owned by 1.7.10, when there is no override but 1.12.2 calls isNormalCube
 			return super.isNormalCube(world.getBlockState(new BlockPos(x, y, z)), world, new BlockPos(x, y, z));
-		else
+		else // When 1.7.10 calls to a block owned by 1.12.2
 			return original.isNormalCube(world.getBlockState(new BlockPos(x, y, z)), world, new BlockPos(x, y, z));
 	}
 
