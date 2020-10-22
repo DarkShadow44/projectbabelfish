fa6a8cd80238a1a1e8c13470b62f542839919926
Fabian Maurer <dark.shadow4@web.de>
2020-10-22 19:34:04 +0200

    Add mixin... for dev environment

---
+++ src/main/java/de/darkshadow44/compatibility/mixin/MixinPropertyEnum.java
@@ -0,0 +1,10 @@
+package de.darkshadow44.compatibility.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+
+import net.minecraft.block.properties.PropertyEnum;
+
+@Mixin(PropertyEnum.class)
+public abstract class MixinPropertyEnum {
+
+}
