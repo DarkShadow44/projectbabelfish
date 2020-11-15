02ddac267044e43bb6ecbcaf8bfaea2429d6e3b8
Fabian Maurer <dark.shadow4@web.de>
2020-11-15 19:35:58 +0100

    GlStateManager: Implement more

--- src/main/java/compat/sandbox/net/minecraft/client/renderer/Compat_GlStateManager.java
+++ src/main/java/compat/sandbox/net/minecraft/client/renderer/Compat_GlStateManager.java
@@ -1,5 +1,7 @@
 package compat.sandbox.net.minecraft.client.renderer;
 
+import java.nio.FloatBuffer;
+
 import net.minecraft.client.renderer.GlStateManager;
 
 public class Compat_GlStateManager {
@@ -63,4 +65,53 @@ public class Compat_GlStateManager {
 	public static void Compat_func_179084_k() {
 		GlStateManager.disableBlend();
 	}
+
+	public static void Compat_func_179088_q() {
+		GlStateManager.enablePolygonOffset();
+	}
+
+	public static void Compat_func_179092_a(int p1, float p2) {
+		GlStateManager.alphaFunc(p1, p2);
+	}
+
+	public static void Compat_func_179101_C() {
+		GlStateManager.disableRescaleNormal();
+	}
+
+	public static void Compat_func_179109_b(float x, float y, float z) {
+		GlStateManager.translate(x, y, z);
+	}
+
+	public static void Compat_func_179110_a(FloatBuffer p1) {
+		GlStateManager.multMatrix(p1);
+	}
+
+	public static void Compat_func_179113_r() {
+		GlStateManager.disablePolygonOffset();
+	}
+
+	public static void Compat_func_179118_c() {
+		GlStateManager.disableAlpha();
+	}
+
+	public static void Compat_func_179120_a(int p1, int p2, int p3, int p4) {
+		GlStateManager.tryBlendFuncSeparate(p1, p2, p3, p4);
+	}
+
+	public static void Compat_func_179136_a(float p1, float p2) {
+		GlStateManager.doPolygonOffset(p1, p2);
+	}
+
+	public static void Compat_func_179139_a(double x, double y, double z) {
+		GlStateManager.scale(x, y, z);
+	}
+
+	public static void Compat_func_179141_d() {
+		GlStateManager.enableAlpha();
+	}
+
+	public static void Compat_func_179152_a(float x, float y, float z) {
+		GlStateManager.scale(x, y, z);
+	}
+
 }
--- src/main/java/compat/sandbox/net/minecraft/client/renderer/Compat_GlStateManager_DestFactor.java
+++ src/main/java/compat/sandbox/net/minecraft/client/renderer/Compat_GlStateManager_DestFactor.java
@@ -5,6 +5,7 @@ import net.minecraft.client.renderer.GlStateManager.DestFactor;
 public enum Compat_GlStateManager_DestFactor {
 
 	ONE_MINUS_SRC_ALPHA(DestFactor.ONE_MINUS_SRC_ALPHA),
+	ONE_MINUS_DST_COLOR(DestFactor.ONE_MINUS_DST_COLOR),
 	ZERO(DestFactor.ZERO);
 
 	private DestFactor original;
@@ -23,5 +24,9 @@ public enum Compat_GlStateManager_DestFactor {
 
 	public static Compat_GlStateManager_DestFactor Compat_get_ZERO() {
 		return ZERO;
+	}
+
+	public static Compat_GlStateManager_DestFactor Compat_get_ONE_MINUS_DST_COLOR() {
+		return ONE_MINUS_DST_COLOR;
 	}
 }
