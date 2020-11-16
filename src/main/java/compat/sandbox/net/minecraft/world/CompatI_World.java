5cc79b7511eb4d1bf7a640cc304a7ca54b76e0d5
Fabian Maurer <dark.shadow4@web.de>
2020-11-16 21:52:55 +0100

    World: implement more

--- src/main/java/compat/sandbox/net/minecraft/world/CompatI_World.java
+++ src/main/java/compat/sandbox/net/minecraft/world/CompatI_World.java
@@ -1,16 +1,26 @@
 package compat.sandbox.net.minecraft.world;
 
 import java.util.List;
+import java.util.Random;
+
+import com.google.common.base.Predicate;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumParticleTypes;
 import net.minecraft.util.SoundCategory;
 import net.minecraft.util.SoundEvent;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.RayTraceResult;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.world.GameRules;
 import net.minecraft.world.World;
+import net.minecraft.world.WorldProvider;
 import net.minecraft.world.border.WorldBorder;
 
 public interface CompatI_World {
@@ -38,11 +48,49 @@ public interface CompatI_World {
 
 	public void notifyLightSetSuper(BlockPos pos);
 
-	public void markBlockRangeForRenderUpdateSuper(BlockPos real, BlockPos real2);
+	public void markBlockRangeForRenderUpdateSuper(BlockPos start, BlockPos end);
 
 	public WorldBorder getWorldBorderSuper();
 
 	public long getTotalWorldTimeSuper();
 
 	public boolean setBlockToAirSuper(BlockPos pos);
+
+	public void markChunkDirtySuper(BlockPos pos, TileEntity tile);
+
+	public <T extends Entity> List<T> getEntitiesWithinAABBSuper(Class<? extends T> classEntity, AxisAlignedBB bb, Predicate<? super T> filter);
+
+	public int getRedstonePowerSuper(BlockPos pos, EnumFacing facing);
+
+	public void updateComparatorOutputLevelSuper(BlockPos pos, Block block);
+
+	public void spawnParticleSuper(EnumParticleTypes type, double x, double y, double z, double xSpeed, double ySpeed, double zSpeed, int...parameters);
+
+	public void notifyNeighborsOfStateExceptSuper(BlockPos pos, Block block, EnumFacing facing);
+
+	public void removeTileEntitySuper(BlockPos pos);
+
+	public boolean mayPlaceSuper(Block block, BlockPos pos, boolean skipCollisionCheck, EnumFacing facing, Entity entity);
+
+	public void neighborChangedSuper(BlockPos pos, Block block, BlockPos pos2);
+
+	public void scheduleBlockUpdateSuper(BlockPos pos, Block block, int delay, int priority);
+
+	public boolean setBlockStateSuper(BlockPos pos, IBlockState state, int flags);
+
+	public void notifyBlockUpdateSuper(BlockPos pos, IBlockState stateOld, IBlockState stateNew, int flags);
+
+	public boolean spawnEntitySuper(Entity entity);
+
+	public RayTraceResult rayTraceBlocksSuper(Vec3d start, Vec3d end);
+
+	public GameRules getGameRulesSuper();
+
+	public boolean isSideSolidSuper(BlockPos pos, EnumFacing facing);
+
+	public boolean get_restoringBlockSnapshots();
+
+	public Random get_rand();
+
+	public WorldProvider get_provider();
 }
--- src/main/java/compat/sandbox/net/minecraft/world/Compat_World.java
+++ src/main/java/compat/sandbox/net/minecraft/world/Compat_World.java
@@ -2,6 +2,9 @@ package compat.sandbox.net.minecraft.world;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
+
+import com.google.common.base.Predicate;
 
 import compat.autogen.Factory;
 import compat.autogen.Factory.CtorPos;
@@ -9,14 +12,21 @@ import compat.core.ParentSelector;
 import compat.sandbox.net.minecraft.block.Compat_Block;
 import compat.sandbox.net.minecraft.block.state.Compat_IBlockState;
 import compat.sandbox.net.minecraft.block.state.Wrapper_IBlockState;
+import compat.sandbox.net.minecraft.entity.Compat_Entity;
 import compat.sandbox.net.minecraft.entity.player.Compat_EntityPlayer;
+import compat.sandbox.net.minecraft.item.Compat_ItemStack;
 import compat.sandbox.net.minecraft.tileentity.Compat_TileEntity;
+import compat.sandbox.net.minecraft.util.Compat_EnumFacing;
+import compat.sandbox.net.minecraft.util.Compat_EnumParticleTypes;
 import compat.sandbox.net.minecraft.util.Compat_SoundCategory;
 import compat.sandbox.net.minecraft.util.Compat_SoundEvent;
 import compat.sandbox.net.minecraft.util.math.Compat_AxisAlignedBB;
 import compat.sandbox.net.minecraft.util.math.Compat_BlockPos;
+import compat.sandbox.net.minecraft.util.math.Compat_RayTraceResult;
+import compat.sandbox.net.minecraft.util.math.Compat_Vec3d;
 import compat.sandbox.net.minecraft.world.border.Compat_WorldBorder;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.world.World;
@@ -131,4 +141,90 @@ public class Compat_World implements Compat_IBlockAccess {
 	public boolean Compat_func_175698_g(Compat_BlockPos pos) {
 		return wrapper.setBlockToAirSuper(pos.getReal());
 	}
+
+	public void Compat_func_175646_b(Compat_BlockPos pos, Compat_TileEntity tile) {
+		wrapper.markChunkDirtySuper(pos.getReal(), tile.getReal());
+	}
+
+	// TODO?
+	public <T extends Entity> List<Compat_Entity> Compat_func_175647_a(Class<? extends T> classEntity, Compat_AxisAlignedBB bb, Predicate<? super T> filter) {
+		List<T> result = wrapper.getEntitiesWithinAABBSuper(classEntity, bb.getReal(), filter);
+
+		List<Compat_Entity> ret = new ArrayList<>();
+
+		for (Entity e : result) {
+			ret.add(new Compat_Entity(e));
+		}
+
+		return ret;
+	}
+
+	public int Compat_func_175651_c(Compat_BlockPos pos, Compat_EnumFacing facing) {
+		return wrapper.getRedstonePowerSuper(pos.getReal(), facing.getReal());
+	}
+
+	public void Compat_func_175666_e(Compat_BlockPos pos, Compat_Block block) {
+		wrapper.updateComparatorOutputLevelSuper(pos.getReal(), block.getReal());
+	}
+
+	public void Compat_func_175688_a(Compat_EnumParticleTypes type, double x, double y, double z, double xSpeed, double ySpeed, double zSpeed, int... parameters) {
+		wrapper.spawnParticleSuper(type.getReal(), x, y, z, xSpeed, ySpeed, zSpeed, parameters);
+	}
+
+	public void Compat_func_175695_a(Compat_BlockPos pos, Compat_Block block, Compat_EnumFacing facing) {
+		wrapper.notifyNeighborsOfStateExceptSuper(pos.getReal(), block.getReal(), facing.getReal());
+	}
+
+	public void Compat_func_175713_t(Compat_BlockPos pos) {
+		wrapper.removeTileEntitySuper(pos.getReal());
+	}
+
+	public boolean Compat_func_175716_a(Compat_Block block, Compat_BlockPos pos, boolean skipCollisionCheck, Compat_EnumFacing facing, Compat_Entity entity, Compat_ItemStack stack) {
+		return wrapper.mayPlaceSuper(block.getReal(), pos.getReal(), skipCollisionCheck, facing.getReal(), entity.getReal());
+	}
+
+	public void Compat_func_180496_d(Compat_BlockPos pos, Compat_Block block) {
+		wrapper.neighborChangedSuper(pos.getReal(), block.getReal(), pos.getReal()); // TODO?;
+	}
+
+	public void Compat_func_180497_b(Compat_BlockPos pos, Compat_Block block, int delay, int priority) {
+		wrapper.scheduleBlockUpdateSuper(pos.getReal(), block.getReal(), delay, priority);
+	}
+
+	public boolean Compat_func_180501_a(Compat_BlockPos pos, Compat_IBlockState state, int flags) {
+		return wrapper.setBlockStateSuper(pos.getReal(), state.getReal(), flags);
+	}
+
+	public void Compat_func_184138_a(Compat_BlockPos pos, Compat_IBlockState stateOld, Compat_IBlockState stateNew, int flags) {
+		wrapper.notifyBlockUpdateSuper(pos.getReal(), stateOld.getReal(), stateNew.getReal(), flags);
+	}
+
+	public boolean Compat_func_72838_d(Compat_Entity entity) {
+		return wrapper.spawnEntitySuper(entity.getReal());
+	}
+
+	public Compat_RayTraceResult Compat_func_72933_a(Compat_Vec3d start, Compat_Vec3d end) {
+		return Compat_RayTraceResult.getFake(wrapper.rayTraceBlocksSuper(start.getReal(), end.getReal()));
+	}
+
+	public Compat_GameRules Compat_func_82736_K() {
+		return new Compat_GameRules(wrapper.getGameRulesSuper());
+	}
+
+	public Compat_WorldProvider Compat_get_field_73011_w() {
+		return new Compat_WorldProvider(wrapper.get_provider());
+	}
+
+	public Random Compat_get_field_73012_v() {
+		return wrapper.get_rand();
+	}
+
+	public boolean Compat_get_restoringBlockSnapshots() {
+		return wrapper.get_restoringBlockSnapshots();
+	}
+
+	public boolean Compat_isSideSolid(Compat_BlockPos pos, Compat_EnumFacing facing) {
+		return wrapper.isSideSolidSuper(pos.getReal(), facing.getReal());
+	}
+
 }
