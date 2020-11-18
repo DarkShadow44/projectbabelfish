fac9ccdf83f65d27dddaa7d49969f119881e8066
Fabian Maurer <dark.shadow4@web.de>
2020-11-18 21:03:06 +0100

    Implement more

--- src/main/java/compat/sandbox/net/minecraft/command/CompatI_CommandBase.java
+++ src/main/java/compat/sandbox/net/minecraft/command/CompatI_CommandBase.java
@@ -1,7 +1,19 @@
 package compat.sandbox.net.minecraft.command;
 
 import net.minecraft.command.CommandBase;
+import net.minecraft.command.ICommandSender;
+import net.minecraft.server.MinecraftServer;
 
 public interface CompatI_CommandBase {
 	public CommandBase get();
+
+	public void executeSuper(MinecraftServer server, ICommandSender sender, String[] args);
+
+	public boolean checkPermissionSuper(MinecraftServer server, ICommandSender sender);
+
+	public String getNameSuper();
+
+	public String getUsageSuper(ICommandSender sender);
+
+	public int getRequiredPermissionLevelSuper();
 }
--- src/main/java/compat/sandbox/net/minecraft/command/Compat_CommandBase.java
+++ src/main/java/compat/sandbox/net/minecraft/command/Compat_CommandBase.java
@@ -1,11 +1,15 @@
 package compat.sandbox.net.minecraft.command;
 
+import compat.autogen.Callback;
 import compat.autogen.Factory;
 import compat.autogen.Factory.CtorPos;
+import compat.autogen.HasCallback;
 import compat.core.ParentSelector;
 import compat.sandbox.net.minecraft.server.Compat_MinecraftServer;
 import net.minecraft.command.CommandBase;
 import net.minecraft.command.CommandException;
+import net.minecraft.command.ICommandSender;
+import net.minecraft.server.MinecraftServer;
 
 public class Compat_CommandBase {
 	private CompatI_CommandBase wrapper;
@@ -32,23 +36,53 @@ public class Compat_CommandBase {
 		return wrapper.get();
 	}
 
+	@Callback
+	public void execute(MinecraftServer server, ICommandSender sender, String[] args) throws CommandException {
+		Compat_func_184881_a(Compat_MinecraftServer.getFake(server), Compat_ICommandSender.getFake(sender), args);
+	}
+
+	@HasCallback
 	public void Compat_func_184881_a(Compat_MinecraftServer server, Compat_ICommandSender sender, String[] args) throws CommandException {
-		getReal().execute(server.getReal(), Compat_ICommandSender.getReal(sender), args);
+		wrapper.executeSuper(server.getReal(), Compat_ICommandSender.getReal(sender), args);
 	}
 
+	@Callback
+	public boolean checkPermission(MinecraftServer server, ICommandSender sender) {
+		return Compat_func_184882_a(Compat_MinecraftServer.getFake(server), Compat_ICommandSender.getFake(sender));
+	}
+
+	@HasCallback
 	public boolean Compat_func_184882_a(Compat_MinecraftServer server, Compat_ICommandSender sender) {
-		return getReal().checkPermission(server.getReal(), Compat_ICommandSender.getReal(sender));
+		return wrapper.checkPermissionSuper(server.getReal(), Compat_ICommandSender.getReal(sender));
 	}
 
+	@Callback
+	public String getName() {
+		return Compat_func_71517_b();
+	}
+
+	@HasCallback
 	public String Compat_func_71517_b() {
-		return getReal().getName();
+		return wrapper.getNameSuper();
 	}
 
+	@Callback
+	public String getUsage(ICommandSender sender) {
+		return Compat_func_71518_a(Compat_ICommandSender.getFake(sender));
+	}
+
+	@HasCallback
 	public String Compat_func_71518_a(Compat_ICommandSender sender) {
-		return getReal().getUsage(Compat_ICommandSender.getReal(sender));
+		return wrapper.getUsageSuper(Compat_ICommandSender.getReal(sender));
 	}
 
+	@Callback
+	public int getRequiredPermissionLevel() {
+		return Compat_func_82362_a();
+	}
+
+	@HasCallback
 	public int Compat_func_82362_a() {
-		return getReal().getRequiredPermissionLevel();
+		return wrapper.getRequiredPermissionLevelSuper();
 	}
 }
