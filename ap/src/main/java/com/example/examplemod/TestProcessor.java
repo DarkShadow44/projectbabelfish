10eb31957d67e02b86c194bc154dd5273d07ad49
Fabian Maurer <dark.shadow4@web.de>
2021-04-17 05:57:07 +0200

    More impl

--- ap/src/main/java/com/example/examplemod/TestProcessor.java
+++ ap/src/main/java/com/example/examplemod/TestProcessor.java
@@ -1,5 +1,6 @@
 package com.example.examplemod;
 
+import java.io.IOException;
 import java.util.Set;
 
 import javax.annotation.processing.AbstractProcessor;
@@ -8,8 +9,16 @@ import javax.annotation.processing.SupportedAnnotationTypes;
 import javax.annotation.processing.SupportedSourceVersion;
 import javax.lang.model.SourceVersion;
 import javax.lang.model.element.Element;
+import javax.lang.model.element.ElementKind;
+import javax.lang.model.element.ExecutableElement;
+import javax.lang.model.element.Modifier;
+import javax.lang.model.element.PackageElement;
 import javax.lang.model.element.TypeElement;
 import javax.tools.Diagnostic;
+
+import com.squareup.javapoet.JavaFile;
+import com.squareup.javapoet.MethodSpec;
+import com.squareup.javapoet.TypeSpec;
 
 @SupportedAnnotationTypes("com.example.examplemod.TestAnnotation")
 @SupportedSourceVersion(SourceVersion.RELEASE_8)
@@ -17,12 +26,46 @@ public class TestProcessor extends AbstractProcessor {
 
 	@Override
 	public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
-		processingEnv.getMessager().printMessage(Diagnostic.Kind.ERROR, "HI");
 		for (TypeElement annotation : annotations) {
 			for (Element element : roundEnv.getElementsAnnotatedWith(annotation)) {
-				processingEnv.getMessager().printMessage(Diagnostic.Kind.NOTE, "found @Log at " + element);
+				processingEnv.getMessager().printMessage(Diagnostic.Kind.NOTE, "Generating class for " + element);
+				try {
+					generateClass((TypeElement) element);
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
 			}
 		}
 		return true;
+	}
+
+	private void generateClass(TypeElement typeIface) throws IOException {
+		String className = typeIface.getSimpleName().toString();
+		className = className.substring(1) + "Impl";
+
+		TypeSpec.Builder builderClass = TypeSpec.classBuilder(className).addModifiers(Modifier.PUBLIC);
+		builderClass.addSuperinterface(typeIface.asType());
+
+		for (Element method : typeIface.getEnclosedElements()) {
+			if (method.getKind() != ElementKind.METHOD)
+				continue;
+
+			builderClass.addMethod(generateMethod((ExecutableElement)method));
+		}
+
+		String packageName = ((PackageElement) typeIface.getEnclosingElement()).getQualifiedName().toString();
+		packageName = "generated." + packageName;
+		JavaFile javaFile = JavaFile.builder(packageName, builderClass.build()).build();
+
+		javaFile.writeTo(System.out);
+		javaFile.writeTo(processingEnv.getFiler());
+	}
+
+	private MethodSpec generateMethod(ExecutableElement method) {
+
+		MethodSpec.Builder builderMethod = MethodSpec.overriding(method);
+		builderMethod.addStatement("return 0");
+
+		return builderMethod.build();
 	}
 }
--- src/main/java/com/example/examplemod/ITest.java
+++ src/main/java/com/example/examplemod/ITest.java
@@ -1,6 +1,8 @@
 package com.example.examplemod;
 
+import java.util.List;
+
 @TestAnnotation
 public interface ITest {
-
+	public int testMethod(int x, List<String> strings);
 }
