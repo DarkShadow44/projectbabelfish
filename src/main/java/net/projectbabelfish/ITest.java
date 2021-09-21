a661925eafdd83195f30445e89a964814f1448c3
unknown <UNSET>
2021-09-21 14:57:55 +0200

    Init 1.17

---
+++ src/main/java/net/projectbabelfish/ITest.java
@@ -0,0 +1,10 @@
+package net.projectbabelfish;
+
+import java.util.List;
+
+import net.projectbabelfish.ap.TestAnnotation;
+
+@TestAnnotation
+public interface ITest {
+	public int testMethod2(int x, List<String> strings);
+}
